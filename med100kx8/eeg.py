7df2944175a251537f0b7c9b257d5a37585e5816
danosb <d.s.osburn@gmail.com>
2023-04-27 14:55:13 -0400

    comment upate

--- med100kx8/eeg.py
+++ med100kx8/eeg.py
@@ -5,3 +5,7 @@ import time
 def eeg_data():
 
     print(f"Starting: {time.time()}")
+    # I will use Node-Red to create a node that receives data the Emotiv EEG including not just brainwave.. 
+    # data  but also metrics that include excitement, interest, engagement, stress, focus, relaxation
+    # Then I'll use the Mosquitto messaging framework to send to Python.
+    # I've got this streaming working, but need to figureo out how to manage so much data coming in.
